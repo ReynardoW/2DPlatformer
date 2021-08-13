1ca076c5fea8855437bb75c3e26a64fc3149e3df
ReynardoW <reybwidjaja@gmail.com>
2021-08-13 17:16:12 -0400

    Added Start Level, and next level portals
    Main Menu:
    Created a new main menu.
    Player is free to move around.
    Big start button on player's screen, upon click it will load the first level.
    
    Exit Portal:
    Created a new game object that once the player collides with the hitbox it will load the next level.

---
+++ 2DPlatformer/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float LevelLoadDelay = 1f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(LevelLoadDelay);
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
